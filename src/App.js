4f247134b3d24509b98c0b4ad25afb14a791fdc6
gemmasmedley90 <gemmasmedley90@gmail.com>
2025-03-18 17:09:08 +0000

    final update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import logo from './logo.jpg';
-import './App.css';
+import logo from "./logo.png";
+import "./App.css";
 import Dictionary from "./Dictionary";
-import React from 'react';
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <header className="App-header">
-      <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
-      <main>
-        <Dictionary />
-      </main>
-      <footer className="App-footer"><small>Coded by Shecodes</small></footer>
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary defaultKeyword="sunset" />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Gemma Smedley</small>
+        </footer>
       </div>
     </div>
   );
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,16 +1,13 @@
 import React from "react";
+import "./Phonetic.css";
 
 export default function Phonetic(props) {
-  console.log(props.phonetic);
   return (
     <div className="Phonetic">
-      <strong>Phonetics:</strong>
-      <br />
-      <a href={props.phonetic.audio} target="_blank" without rel="noreferrer">
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         Listen
       </a>
-      <br />
-      {props.phonetic.text}
+      <span className="text">{props.phonetic.text}</span>
     </div>
   );
 }
